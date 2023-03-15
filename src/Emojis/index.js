60958231c72d0a3996b1510a50fb08a19532e1d2
Lucija <lucia.hranjec@gmail.com>
2023-03-15 22:32:48 +0100

    Finished emoji picker style, deleted old icons

--- src/Emojis/index.js
+++ src/Emojis/index.js
@@ -1,40 +1,30 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "../util/fetch";
-import './emojis.scss';
+import "./emojis.scss";
 
-const Emoji = () => {
+const Emoji = ({ handleEmojiClick }) => {
     const [selectedCategory, setSelectedCategory] = useState("smileys_emotion");
     const [emojiData, setEmojiData] = useState([]);
-
-    // const map = (data) => {
-    //     for (let category of data) {
-    //         console.log(category.slug);
-    //     }
-    // };
 
     const filterAndSetEmojiData = (data) => {
         const allFilteredCategories = [];
 
         for (const category of data) {
-            const dataByCategory = {slug: category.slug, emojis: []};
+            const dataByCategory = { slug: category.slug, emojis: [] };
 
             for (const emojiObject of category.emojis) {
-                const filteredEmojiObject = {name: emojiObject.name, emoji: emojiObject.emoji}
+                const filteredEmojiObject = {
+                    name: emojiObject.name,
+                    emoji: emojiObject.emoji,
+                };
                 dataByCategory.emojis.push(filteredEmojiObject);
-            };
+            }
 
             allFilteredCategories.push(dataByCategory);
-        };
+        }
 
         setEmojiData([...allFilteredCategories]);
     };
-
-    // const x = () => {
-    //     console.log("it works", emojiData);
-    // };
-    // x(emojiData.map((emoji) => {
-                    
-                // }));
 
     useEffect(() => {
         fetchData("./json/emojis.json", filterAndSetEmojiData);
@@ -46,89 +36,118 @@ const Emoji = () => {
             if (category.slug === selectedCategory) {
                 selectedCategoryData.push(...category.emojis);
                 break;
-                // const emojiList = category.emojis.map((emojiObject) => {
-                //     <li className="emoji" key={emojiObject.name}>
-                //         {emojiObject.emoji}
-                //     </li>
-                }
-            };
+            }
+        }
 
-        return (selectedCategoryData.map((emojiObject) => {
+        return selectedCategoryData.map((emojiObject) => {
             return (
                 <li className="emoji" key={emojiObject.name}>
                     {emojiObject.emoji}
                 </li>
             );
-        }));
+        });
     };
- 
+
     const handleEmojiCategories = (e) => {
         e.stopPropagation();
-        const categorySlugDataset = e.target.dataset.categorySlug;
-        setSelectedCategory(categorySlugDataset);
+        if (e.target.dataset.categorySlug) {
+            const categorySlugDataset = e.target.dataset.categorySlug;
+            setSelectedCategory(categorySlugDataset);
+        }
     };
 
     return (
         <div className="emoji-container">
-            <ul className="emoji-categories-list" onClick={handleEmojiCategories}>
-                <li className="emoji-category">
+            <ul
+                className="emoji-categories-list"
+                onClick={handleEmojiCategories}
+            >
+                <li
+                    className="emoji-category"
+                    data-category-slug="smileys_emotion"
+                >
                     <img
-                        src="./emoji-bar-icons/smileys.png"
+                        src="./emoji-bar-icons/smileys.svg"
                         alt="Smileys & Emotions"
                         data-category-slug="smileys_emotion"
                     />
                 </li>
-                <li className="emoji-category">
+                <li 
+                    className="emoji-category" 
+                    data-category-slug="people_body"
+                >
                     <img
-                        src="./emoji-bar-icons/people.png"
+                        src="./emoji-bar-icons/people.svg"
                         alt="People & Body"
                         data-category-slug="people_body"
                     />
                 </li>
-                <li className="emoji-category">
+                <li
+                    className="emoji-category"
+                    data-category-slug="animals_nature"
+                >
                     <img
-                        src="./emoji-bar-icons/animals.png"
+                        src="./emoji-bar-icons/animals.svg"
                         alt="Animals & Nature"
                         data-category-slug="animals_nature"
                     />
                 </li>
-                <li className="emoji-category">
+                <li 
+                    className="emoji-category" 
+                    data-category-slug="food_drink"
+                >
                     <img
-                        sr="./emoji-bar-icons/flags.png"
+                        src="./emoji-bar-icons/food.svg"
                         alt="Food & Drink"
                         data-category-slug="food_drink"
                     />
                 </li>
-                <li className="emoji-category">
+                <li
+                    className="emoji-category"
+                    data-category-slug="travel_places"
+                >
                     <img
-                        src="./emoji-bar-icons/travel.png"
+                        src="./emoji-bar-icons/travel.svg"
                         alt="Travel & Places"
                         data-category-slug="travel_places"
                     />
                 </li>
-                <li className="emoji-category">
+                <li 
+                    className="emoji-category" 
+                    data-category-slug="activities"
+                >
                     <img
-                        src="./emoji-bar-icons/activities.png"
+                        src="./emoji-bar-icons/activities.svg"
                         alt="Activities"
                         data-category-slug="activities"
                     />
                 </li>
-                <li className="emoji-category">
+                <li 
+                    className="emoji-category" 
+                    data-category-slug="objects"
+                >
                     <img
-                        src="./emoji-bar-icons/objects.png"
+                        src="./emoji-bar-icons/objects.svg"
                         alt="Objects"
                         data-category-slug="objects"
                     />
                 </li>
-                <li className="emoji-category">
+                <li 
+                    className="emoji-category" 
+                    data-category-slug="symbols"
+                >
                     <img
-                        src="./emoji-bar-icons/symbols.png"
+                        src="./emoji-bar-icons/symbols.svg"
                         alt="Symbols"
                         data-category-slug="symbols"
                     />
                 </li>
             </ul>
-            <ul className="emoji-list" id="emoji-list">
+            <ul
+                className="emoji-list"
+                id="emoji-list"
+                onClick={handleEmojiClick}
+            >
                 {displaySelectedEmojiCategory()}
             </ul>
         </div>
